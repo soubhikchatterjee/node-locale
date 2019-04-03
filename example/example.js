5fe8465c8929c93eceadeafd3bdc74c308411f99
Soubhik Chatterjee <soubhik.chatterjee@eisenvault.com>
2019-04-03 16:46:33 +0530

    Add support for chaanging module and local at run-time

--- example/example.js
+++ example/example.js
@@ -8,9 +8,18 @@ var t = new T({
 });
 
 console.log(t._('simple')); // Thanks for using this package
+console.log(t._('greetings')); // Have a good day!
 
 // Can also load nested JSON objects
 console.log(t._('nested').a.b); // Yay!
 
 // Supports arguments
 console.log(t._('advanced', ['John', 'nodejs-locale'])); // Thanks John, for using nodejs-locale!!!!!!
+
+// Change the locale at run-time
+t.locale = 'fr'
+
+// Change the module name at run-time
+t.module = 'users';
+
+console.log(t._('greetings')); // Bonne journée!
--- index.js
+++ index.js
@@ -4,29 +4,46 @@ const fs = require('fs');
 class Locale {
 
     constructor({ locale, module, dir }) {
-        this.locale = locale;
-        this.module = module;
-        this.dir = dir;
+        this._locale = locale;
+        this._module = module;
+        this._dir = dir;
     }
 
     _(key, args = []) {
         const message = this._message(key);
-
         const final = message.split(' ').map(item => {
-          if (item.match(/{{.}}/)) {
-              const getIndex = item.match(/\d/);
+            if (item.match(/{{.}}/)) {
+                const getIndex = item.match(/\d/);
 
-              if(!getIndex) {
-                 return '';
-              }
+                if (!getIndex) {
+                    return '';
+                }
 
-              return item.replace(/{{.}}/g, args[getIndex]);
-          }
+                return item.replace(/{{.}}/g, args[getIndex]);
+            }
 
-          return item;
+            return item;
         });
 
         return final.join(' ');
+    }
+
+    get module() {
+        return this._module;
+    }
+
+    set module(newModule) {
+        this._module = newModule;
+        this._loadResource(); // Invalidate cache
+    }
+
+    get locale() {
+        return this._locale;
+    }
+
+    set locale(newLocale) {
+        this._locale = newLocale;
+        this._loadResource(); // Invalidate cache
     }
 
     _message(key) {
@@ -40,13 +57,12 @@ class Locale {
     }
 
     _loadResource() {
-        if (!this.dir) {
-            this.dir = path.join(__dirname, 'resources', 'locale');
+        if (!this._dir) {
+            this._dir = path.join(__dirname, 'resources', 'locale');
         }
 
-        this.dir = path.join(this.dir, this.locale);
-
-        const langFile = path.join(this.dir, `${this.module}.json`);
+        const dir = path.join(this._dir, this._locale);
+        const langFile = path.join(dir, `${this._module}.json`);
 
         if (!fs.existsSync(langFile)) {
             throw new Error(`Language file ${langFile} does not exists`);
